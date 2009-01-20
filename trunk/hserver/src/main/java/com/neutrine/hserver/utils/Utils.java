dc7f43515eff53146c8aeffe3427b6d75e5528e5
lpicanco <lpicanco@927538a6-ddf2-11dd-a550-c997509ed985>
2009-01-20 03:56:31 +0000

    Refactoring no nome dos pacotes

---
+++ trunk/hserver/src/main/java/com/neutrine/hserver/utils/Utils.java
@@ -0,0 +1,23 @@
+package com.neutrine.hserver.utils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+/**
+ *
+ * @author Ulysses Souza
+ */
+public class Utils {
+    public static void sendBytes(InputStream fis, OutputStream os) throws IOException {
+        byte[] buffer = new byte[1024];
+        int bytes = 0;
+        while ( (bytes = fis.read(buffer) ) != -1) {
+            os.write(buffer, 0, bytes);
+        }
+    }
+
+    public static void sendBytes(byte[] bytes, OutputStream os) throws IOException {
+        os.write(bytes, 0, bytes.length);
+    }
+}
